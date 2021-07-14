b8f26acc66b23fcf36479818fd0f1f5b0b862b3e
Aldy Akbarrizky <aldyakbarrizky18@gmail.com>
2021-07-14 08:12:51 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Function Point</title>
+</head>
+<body>
+    <div class="container-fluid fill-height" style="background-color: #AAA">
+        <div class="main-content">
+            <h4>Program Perhitungan Function Point</h4><br/>
+            <span>Pilih jenis Function Point</span>
+            <select class="form-select select-fp">
+                <option value="1">Development Project Function Point Count</option>
+                <option value="2">Enhancement Project Function Point Count</option>
+                <option value="3">Application Function Point Count</option>
+            </select>
+            <button type="button" class="btn btn-primary">Lanjutkan</button>
+        </div>
+    </div>
+</body>
+</html>
